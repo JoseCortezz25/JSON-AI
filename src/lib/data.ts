d080df3f1831d7a704934d6250adf630dabc2645
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2024-04-29 19:41:52 -0500

    Add data.ts file with examples array

---
+++ src/lib/data.ts
@@ -0,0 +1,30 @@
+export const examples = [
+  {
+    id: 1,
+    label: 'List of Rick and Morty characters'
+  },
+  {
+    id: 2,
+    label: 'List of Pokémon'
+  },
+  {
+    id: 3,
+    label: 'Top science fiction books read in 2020'
+  },
+  {
+    id: 4,
+    label: 'List of random legendary actors who had role in Hollywood'
+  },
+  {
+    id: 5,
+    label: 'Object-oriented programming languages'
+  },
+  {
+    id: 6,
+    label: 'Generate random news about sport topic'
+  },
+  {
+    id: 7,
+    label: 'Get the top 10 countries in Europe'
+  }
+];
